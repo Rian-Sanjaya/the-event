42404b601a84d131d74da731714950ae21130037
Rian <rian.sj@gmail.com>
2019-08-04 08:41:21 +0700

    add build

---
+++ build/precache-manifest.edb033503765de50efc435b07610d709.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9664fce65fd6011f009fb863919ff37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "802b43d5cd3a0783daeb",
+    "url": "/static/css/main.58c2c38a.chunk.css"
+  },
+  {
+    "revision": "7c7a7bf563d0d599ff98",
+    "url": "/static/js/2.41e52d5a.chunk.js"
+  },
+  {
+    "revision": "802b43d5cd3a0783daeb",
+    "url": "/static/js/main.c0268221.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a9bf98be61122838af29bd8e1332e6d9",
+    "url": "/static/media/slide_one.resized.a9bf98be.jpg"
+  },
+  {
+    "revision": "b611996f8cbb0c50d23317e62ec82c63",
+    "url": "/static/media/slide_three.resized.b611996f.jpg"
+  },
+  {
+    "revision": "312e2624af7d98bd553eef15f8c6db40",
+    "url": "/static/media/slide_two.resized.312e2624.jpg"
+  }
+]);
